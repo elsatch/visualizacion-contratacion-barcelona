a1446c80d668fd15f1be6081bc4f8c846f865682
César García Sáez <cesargarciasaez@gmail.com>
2023-01-23 00:08:33 +0100

    Métricas en columnas

--- contratacion_barcelona.py
+++ contratacion_barcelona.py
@@ -25,11 +25,16 @@ st.write('En primer lugar, vamos a ver cuántos contratos se han realizado en Ba
 #st.write(minor_contracts_df.groupby('organo_gestor').agg({'importe': 'sum', 'id': 'count'}).rename(columns={'id': 'contratos'}).sort_values('contratos', ascending=False))
 
 st.write(df.head())
-contratos_2020 = df[df['Año'] == '2020'].count()
-contratos_2021 = df[df['Año'] == '2021'].count()
+
+# Extract the number of contracts per year, adding the column 'Número' per year
+contratos_2020 = df[df['año'] == 2020]['Número'].sum()
+contratos_2021 = df[df['año'] == 2021]['Número'].sum()
+
 st.write(contratos_2020)
 st.write(contratos_2021)
-st.metric(label="Num. Contratos 2021", value=contratos_2020)
-st.metric(label="Num. Contratos 2020", value=contratos_2021)
+
+col1, col2 = st.columns(2)
+col1.metric(label="Num. Contratos 2021", value=contratos_2020, delta='4%')
+col2.metric(label="Num. Contratos 2020", value=contratos_2021, delta='7$')
 
 st.plotly_chart(px.histogram(df, x='año', title='Número de contratos por año'))
