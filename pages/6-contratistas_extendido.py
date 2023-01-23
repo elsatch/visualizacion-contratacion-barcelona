1d394339d2fe148a98b361216635164226e7cef7
César García Sáez <cesargarciasaez@gmail.com>
2023-01-23 23:17:21 +0100

    Typo contratistas

--- pages/6-contratistas_extendido.py
+++ pages/6-contratistas_extendido.py
@@ -20,7 +20,7 @@ contractors_ext_df = load_data(df_file)
 st.title("Analisis extendido de contratistas del Ayuntamiento de Barcelona")
 st.subheader("Visualización de datos abiertos de contratación en Barcelona")
 
-st.write("En esta página vamos a ver poder explorar en mas detallos los datos de los contratistas del Ayuntamiento de Barcelona en 2021.")
+st.write("En esta página vamos a poder explorar en más detallos los datos de los contratistas del Ayuntamiento de Barcelona en 2021.")
 
 # Dibujamos un treemap con los datos de los contratistas extendidos
 st.plotly_chart(px.treemap(contractors_ext_df, path=['tipus_contractes','nom_adjudicatari'], values='import_adjudicacio_iva', color='import_adjudicacio_iva', color_continuous_scale='RdBu', title='Analisis extendido de los contratistas por tipo de contrato e importe total'))
