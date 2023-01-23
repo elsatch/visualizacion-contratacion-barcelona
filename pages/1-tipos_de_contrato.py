9a4f3b7046d910305caf956c3e7818daca084662
César García Sáez <cesargarciasaez@gmail.com>
2023-01-23 23:07:58 +0100

    v1337

--- pages/1-tipos_de_contrato.py
+++ pages/1-tipos_de_contrato.py
@@ -15,9 +15,9 @@ def load_data(file):
 df = load_data(df_file)
 
 # Creamos el formato y lo aplicamos, pero no lo mostramos
-fig = px.bar(df, x="Procedimiento", y="Número", color="Procedimiento", title="Número de contratos por procedimiento")
-fig.update_traces(texttemplate='%{text:.2s}', textposition='outside', textangle=45)
-fig.update_layout(barmode='group', xaxis_tickangle=-45)
+fig1 = px.bar(df, x="Procedimiento", y="Número", color="Procedimiento", title="Número de contratos por procedimiento")
+fig1.update_traces(texttemplate='%{text:.2s}', textposition='outside', textangle=45)
+fig1.update_layout(barmode='group', xaxis_tickangle=-45)
 
 st.title("Procedimientos y tipos de procedimientos de contratos públicos")
 st.subheader("Visualización de datos abiertos de contratación en Barcelona")
@@ -31,7 +31,8 @@ st.write('En la siguiente gráfica vamos a ver el número de contratos realizado
 # Personalizamos el formato de las gráficas para que se vean mejor
 # fig = px.bar(df, x="Procedimiento", y="Número", color="Procedimiento", title="Número de contratos por procedimiento")
 
-st.plotly_chart(px.bar(df, x="Procedimiento", y="Número", color="Procedimiento", title="Número de contratos por procedimiento"))
+#st.plotly_chart(px.bar(df, x="Procedimiento", y="Número", color="Procedimiento", title="Número de contratos por procedimiento"))
+st.plotly_chart(fig1)
 
 st.write('En la siguiente gráfica vamos a ver importe de los contratos realizados, agrupados por procedimiento')
 st.plotly_chart(px.bar(df, x="Procedimiento", y="Importe", color="Procedimiento", title="Número de contratos por tipo"))
