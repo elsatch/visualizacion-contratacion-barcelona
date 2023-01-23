f82a17c8fb079b17c0aebba1da7b59b46f2baf1b
César García Sáez <cesargarciasaez@gmail.com>
2023-01-23 18:06:51 +0100

    Creado Treemap para obtener una visión general

--- contratacion_barcelona.py
+++ contratacion_barcelona.py
@@ -51,4 +51,11 @@ st.write('¿Cómo se reparten estos contratos entre los distintos tipos de entid
 st.plotly_chart(px.histogram(df, x='Tipo_entidad', y='Importe', color='Tipo_entidad', title='Importe por Tipo de entidad'))
 
 st.write('A continuación vamos a visualizar el número de contratos realizados por entidad, independientemente de su importe')
-st.plotly_chart(px.histogram(df, x='Nombre_entidad', y='Número', color='Nombre_entidad', title='Número de contratos por entidad'))
+st.plotly_chart(px.histogram(df, x='Nombre_entidad', y='Número', color='Tipo_entidad', title='Número de contratos por entidad'))
+
+st.write('El Ayuntamiento de Barcelona, es la entidad que realiza mayor número de contratos y por un mayor importe. Aún así, podemos observar que el resto de entidades gestionan un tercio del volumen total de compras (por importe')
+st.write('Dado que estamos explorando los distintos contratos te aniamos a explorar el siguiente gráfico interactivo para saber más sobre los contratos realizados por el Ayuntamiento de Barcelona en 2021')
+
+st.plotly_chart(px.treemap(df, path=['Tipo_entidad', 'Nombre_entidad', 'Tipo de contrato'], values='Importe', color='Importe', color_continuous_scale='RdBu', title='Contratos realizados por el Ayuntamiento de Barcelona en 2021'))
+
+
